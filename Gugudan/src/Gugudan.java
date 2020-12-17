31bf93bf0fdea74b2d434688b288b162d424e532
aksmf1442 <64648893+aksmf1442@users.noreply.github.com>
2020-12-17 15:55:08 +0900

    자바 구구단 공부 완료
    프로그램을 시작하는 Gugudanmain클래스와 구구단을 구현하는 Gugudan클래스를 분리하여
    구구단을 구현하였다.

---
+++ Gugudan/src/Gugudan.java
@@ -0,0 +1,17 @@
+
+public class Gugudan {
+    public static int[] calculate(int times){
+        int[] result = new int[9];
+        for (int i = 0; i < result.length; i++){
+            result[i] = times * (i+1);
+        }
+
+        return result;
+    }
+    public static void print(int[] result){
+        System.out.println(result[0]+"단");
+        for (int i = 0; i < result.length; i++){
+            System.out.printf("%d*%d=%d%n",result[0],i+1,result[i]);
+        }
+    }
+}
