a08375bc602b1b30296cc91b21e288b979ca7d2e
aksmf1442 <64648893+aksmf1442@users.noreply.github.com>
2020-12-18 17:30:49 +0900

    Add repeat function

--- Calendar/src/Calendar.java
+++ Calendar/src/Calendar.java
@@ -1,13 +1,13 @@
 import java.util.Scanner;
 
 public class Calendar {
-    private static final int[] MAX_DAYS = {31, 28, 31, 30, 31, 30, 31, 31 ,30, 31, 30, 31};
+    private static final int[] MAX_DAYS = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
-    public int getMaxDaysOfMonth(int month){
-        return MAX_DAYS[month-1];
+    public int getMaxDaysOfMonth(int month) {
+        return MAX_DAYS[month - 1];
     }
 
-    public void printSampleCalendar(){
+    public void printSampleCalendar() {
         System.out.println("일  월  화  수  목  금  토");
         System.out.println("----------------------");
         System.out.println(" 1  2  3  4  5  6  7");
@@ -16,18 +16,21 @@ public class Calendar {
         System.out.println("22 23 24 25 26 27 28");
     }
 
-    public static void main(String[] args){
-
-        System.out.println("달을 입력하세요.");
+    public static void main(String[] args) {
+        System.out.println("반복 횟수를 입력하세요.");
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        System.out.println("월을 입력하세요.");
         // 객체 생성
         Calendar cal = new Calendar();
-        Scanner scanner = new Scanner(System.in);
-        int month = scanner.nextInt();
-        if (month < 13 && month > 0){
-            System.out.printf("%d월은 %d일까지 있습니다.%n", month, cal.getMaxDaysOfMonth(month));
-        }else{
-            System.out.printf("%d월은 없는 달입니다.",month);
+        for (int i = 0; i < n; i++) {
+            int month = scanner.nextInt();
+            if (month < 13 && month > 0) {
+                System.out.printf("%d월은 %d일까지 있습니다.%n", month, cal.getMaxDaysOfMonth(month));
+            } else {
+                System.out.printf("%d월은 없는 달입니다.", month);
+            }
         }
-        cal.printSampleCalendar();
+        System.out.println("Bye~");
     }
 }
