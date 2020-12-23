b1c49624859d6bd920a221e2a8c813a07010fe4f
aksmf1442 <64648893+aksmf1442@users.noreply.github.com>
2020-12-23 13:49:28 +0900

    Implement RpgGame

--- RpgGame/src/PlayGame.java
+++ RpgGame/src/PlayGame.java
@@ -19,16 +19,17 @@ import java.util.Scanner;
 
 public class PlayGame {
     private static String[][] gameMap = new String[11][11];
+    private static String[][] gameMineMap = new String[11][11];
     private final int INIT_LOCATION = 5;
     private final int[] moveX = {-1,0,1,0};
     private final int[] moveY = {0,-1,0,1};
     private final String background = "*";
     private final String player = "P";
-    private final String mine = "❌";
+    private final String mine = "X";
     private final String monster = "M";
     private final int[] playerXY = new int[2];
-    private final int[] mineXY = new int[2];
     private final int[] monsterXY = new int[2];
+    private int minecount = 1;
     private int result = 0;
 
     public static void main(String[] args){
@@ -46,29 +47,75 @@ public class PlayGame {
         }
     }
 
-    private void MoveW(){
+    private void printMineMap(){
+        for (String[] i : gameMineMap) {
+            for (String j : i) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+
+    private Boolean MoveW(){
         gameMap[playerXY[0]][playerXY[1]] = background;
+        gameMineMap[playerXY[0]][playerXY[1]] = background;
         playerXY[0] += moveX[0];
         playerXY[1] += moveY[0];
+        if (gameMineMap[playerXY[0]][playerXY[1]].equals(mine)) return false;
         gameMap[playerXY[0]][playerXY[1]] = player;
+        gameMineMap[playerXY[0]][playerXY[1]] = player;
+        return true;
     }
-    private void MoveA(){
+    private Boolean MoveA(){
         gameMap[playerXY[0]][playerXY[1]] = background;
+        gameMineMap[playerXY[0]][playerXY[1]] = background;
         playerXY[0] += moveX[1];
         playerXY[1] += moveY[1];
+        if (gameMineMap[playerXY[0]][playerXY[1]].equals(mine)) return false;
         gameMap[playerXY[0]][playerXY[1]] = player;
+        gameMineMap[playerXY[0]][playerXY[1]] = player;
+        return true;
     }
-    private void MoveS(){
+    private Boolean MoveS(){
         gameMap[playerXY[0]][playerXY[1]] = background;
+        gameMineMap[playerXY[0]][playerXY[1]] = background;
         playerXY[0] += moveX[2];
         playerXY[1] += moveY[2];
+        if (gameMineMap[playerXY[0]][playerXY[1]].equals(mine)) return false;
         gameMap[playerXY[0]][playerXY[1]] = player;
+        gameMineMap[playerXY[0]][playerXY[1]] = player;
+        return true;
     }
-    private void MoveD(){
+    private Boolean MoveD(){
         gameMap[playerXY[0]][playerXY[1]] = background;
+        gameMineMap[playerXY[0]][playerXY[1]] = background;
         playerXY[0] += moveX[3];
         playerXY[1] += moveY[3];
+        if (gameMineMap[playerXY[0]][playerXY[1]].equals(mine)) return false;
         gameMap[playerXY[0]][playerXY[1]] = player;
+        gameMineMap[playerXY[0]][playerXY[1]] = player;
+        return true;
+    }
+
+    private boolean errorCheck(String check){
+        if (check.equals("W") && playerXY[0] == 0){
+            System.out.println("경로를 이탈했습니다. 다시 입력해주세요");
+            printMap();
+            return true;
+        }else if (check.equals("A") && playerXY[1] == 0) {
+            System.out.println("경로를 이탈했습니다. 다시 입력해주세요");
+            printMap();
+            return true;
+        }else if (check.equals("S") && playerXY[0] == 10) {
+            System.out.println("경로를 이탈했습니다. 다시 입력해주세요");
+            printMap();
+            return true;
+        }else if (check.equals("D") && playerXY[1] == 10) {
+            System.out.println("경로를 이탈했습니다. 다시 입력해주세요");
+            printMap();
+            return true;
+        }
+        return false;
     }
 
     private void play(){
@@ -77,20 +124,40 @@ public class PlayGame {
             System.out.println("WASD중 하나를 입력해주세요.(몬스터를 잡아주세요. 중간에 지뢰가 숨어있고, 밟으면 게임이 종료됩니다.)");
             System.out.print("> ");
             String direction = scanner.next();
-            if (direction.equals("W")) MoveW();
-            else if(direction.equals("A")) MoveA();
-            else if(direction.equals("S")) MoveS();
-            else if(direction.equals("D")) MoveD();
+            Boolean is_true= true;
+            if (direction.equals("W")) {
+                if (errorCheck("W")) continue;
+                is_true = MoveW();
+            }
+            else if(direction.equals("A")) {
+                if (errorCheck("A")) continue;
+                is_true = MoveA();
+            }
+            else if(direction.equals("S")) {
+                if (errorCheck("S")) continue;
+                is_true = MoveS();
+            }
+            else if(direction.equals("D")) {
+                if (errorCheck("D")) continue;
+                is_true = MoveD();
+            }
             else System.out.println("WASD중에 하나를 입력해주세요.");
-            if (playerXY[0] == mineXY[0] && playerXY[1] == mineXY[1] ) {
+            if (!is_true) {
+                printMineMap();
                 System.out.printf("지뢰를 밟아서 게임을 종료합니다.(처치한 몬스터 수:%d)\n",result);
                 break;
             }
             printMap();
             if (playerXY[0] == monsterXY[0] && playerXY[1] == monsterXY[1]){
                 result += 1;
+                minecount *= 2;
+                if (minecount >= 128) {
+                    System.out.printf("%d마리의 몬스터를 잡고, 게임을 클리어 하셨습니다. 축하합니다😆\n", result);
+                    break;
+                }
                 System.out.printf("현재 %d마리의 몬스터를 잡았습니다.\n", result);
                 gameMap = new String[11][11];
+                gameMineMap = new String[11][11];
                 initValue();
             }
         }
@@ -109,21 +176,30 @@ public class PlayGame {
         playerXY[0] = INIT_LOCATION;
         playerXY[1] = INIT_LOCATION;
         gameMap[playerXY[0]][playerXY[1]] = player;
+        gameMineMap[playerXY[0]][playerXY[1]] = player;
     }
 
     private void initMine(Random random){
-        mineXY[0] = random.nextInt(11);
-        mineXY[1] = random.nextInt(11);
-        if (playerXY[0] == mineXY[0] && playerXY[1] == mineXY[1]) initValue();
-        gameMap[mineXY[0]][mineXY[1]] = mine;
+        int x, y;
+        for (int i = 0; i < minecount; i++) {
+            x = random.nextInt(11);
+            y = random.nextInt(11);
+            if (gameMineMap[x][y] == null) {
+                gameMineMap[x][y] = mine;
+            } else {
+                i--;
+            }
+        }
     }
 
     private void initMonster(Random random){
         monsterXY[0] = random.nextInt(11);
         monsterXY[1] = random.nextInt(11);
-        if ((playerXY[0] == monsterXY[0] && playerXY[1] == monsterXY[1]) ||
-                (mineXY[0] == monsterXY[0] && mineXY[1] == monsterXY[1])) initValue();
+        if (gameMineMap[monsterXY[0]][monsterXY[1]] != null){
+            initMonster(random);
+        }
         gameMap[monsterXY[0]][monsterXY[1]] = monster;
+        gameMineMap[monsterXY[0]][monsterXY[1]] = monster;
     }
 
     private void initBackground(){
@@ -131,6 +207,9 @@ public class PlayGame {
             for (int j = 0; j < gameMap.length; j++){
                 if (gameMap[i][j] == null){
                     gameMap[i][j] = background;
+                }
+                if (gameMineMap[i][j] == null){
+                    gameMineMap[i][j] = background;
                 }
             }
         }
