0ed151b94cac8fd234938225bb6c5fcc8cbf82c5
aksmf1442 <64648893+aksmf1442@users.noreply.github.com>
2020-12-26 21:10:48 +0900

    Java Stack, Queue, HashSet 공부

--- InflearnDoItJava/src/collection/arraylist/MemberArrayList.java
+++ InflearnDoItJava/src/collection/arraylist/MemberArrayList.java
@@ -1,7 +1,9 @@
 package collection.arraylist;
 
 import collection.Member;
+
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class MemberArrayList {
 
@@ -15,19 +17,29 @@ public class MemberArrayList {
         arrayList.add(member);
     }
 
-    public boolean removeMember(int memberId){
-        for (int i = 0; i < arrayList.size(); i++){
-            Member member = arrayList.get(i);
+    public boolean removeMember(int memberId) {
+//        for (int i = 0; i < arrayList.size(); i++){
+//            Member member = arrayList.get(i);
+//            int tempId = member.getMemberId();
+//            if (memberId == tempId){
+//                arrayList.remove(i);
+//                return true;
+//            }
+//        }
+
+        Iterator<Member> iterator = arrayList.iterator();
+        while (iterator.hasNext()) {
+            Member member = iterator.next();
             int tempId = member.getMemberId();
-            if (memberId == tempId){
-                arrayList.remove(i);
+            if (memberId == tempId) {
+                arrayList.remove(member);
                 return true;
             }
+
         }
         System.out.println(memberId + "가 존재하지 않습니다.");
         return false;
     }
-
     public void showAll(){
         for (Member member : arrayList){
             System.out.println(member);
