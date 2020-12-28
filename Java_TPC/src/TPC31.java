a65231c7f5771e3b0b3441e3005c046acc2e07f3
aksmf1442 <64648893+aksmf1442@users.noreply.github.com>
2020-12-28 09:12:49 +0900

    Java private생성자 공부

---
+++ Java_TPC/src/TPC31.java
@@ -0,0 +1,19 @@
+import kr.tpc.Inflearn;
+
+public class TPC31 {
+    public static void main(String[] args) {
+        // 생성자가 private이라서 객체를 만들 수 없음.
+//        Inflearn inf = new Inflearn();
+//        inf.tpc();
+        Inflearn.java();
+//        inf.java();
+        Inflearn.java();
+
+        // Java API 생성자 private
+        // System sys = new System();
+        System.out.println("출력");
+        // Math m = new Math();
+        int v = Math.max(10, 20);
+        System.out.println(v);
+    }
+}
