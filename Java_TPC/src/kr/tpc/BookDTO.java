d24da69124d8123bae5105631be81cf82ab5b7a0
aksmf1442 <64648893+aksmf1442@users.noreply.github.com>
2020-12-28 09:32:05 +0900

    Java class, object, instance의 상호관계 공부

--- Java_TPC/src/kr/tpc/BookDTO.java
+++ Java_TPC/src/kr/tpc/BookDTO.java
@@ -5,7 +5,14 @@ public class BookDTO {
     public int price;
     public String company;
     public int page;
-    // 디폴트 생성자 메서드(생략 되어 있음)
-    public BookDTO(){
+    
+    public BookDTO(){};
+
+    public BookDTO(String title, int price, String company, int page) {
+        this.title = title;
+        this.price = price;
+        this.company = company;
+        this.page = page;
     }
+
 }
