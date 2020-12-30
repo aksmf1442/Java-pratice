3bd4d9cf80ff2d31d699077c8d31373d4499d5ca
aksmf1442 <64648893+aksmf1442@users.noreply.github.com>
2020-12-30 09:03:08 +0900

    Refactoring RpgGame MapClass

--- RpgGame/src/Map.java
+++ RpgGame/src/Map.java
@@ -1,2 +1,48 @@
 public class Map {
+    private String[][] gameMap = new String[11][11];
+    private String[][] gameMineMap = new String[11][11];
+    private final String background = "*";
+
+    public Map(){
+        for(int i = 0; i < gameMap.length; i++){
+            for (int j = 0; j < gameMap[0].length; j++){
+                gameMap[i][j] = background;
+                gameMineMap[i][j] = background;
+            }
+        }
+    }
+
+    public String[][] getGameMap() {
+        return gameMap;
+    }
+
+    public void setGameMap(String[][] gameMap) {
+        this.gameMap = gameMap;
+    }
+
+    public String[][] getGameMineMap() {
+        return gameMineMap;
+    }
+
+    public void setGameMineMap(String[][] gameMineMap) {
+        this.gameMineMap = gameMineMap;
+    }
+
+    public void printMap(){
+        for (String[] i : gameMap) {
+            for (String j : i) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+
+    public void printMineMap(){
+        for (String[] i : gameMineMap) {
+            for (String j : i) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
 }
