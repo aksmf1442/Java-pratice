4be3499eabd6bc48585f729c66db5c93468d33d5
aksmf1442 <64648893+aksmf1442@users.noreply.github.com>
2020-12-31 18:19:18 +0900

    Java Ex7_19 complete

--- JavaStandardPractice/src/ch7/Buyer.java
+++ JavaStandardPractice/src/ch7/Buyer.java
@@ -9,16 +9,31 @@ public class Buyer {
        if(money < p.price){
            System.out.printf("잔액이 부족하여 %s을/를 살수 없습니다.\n", p);
        }else{
-           money
+           money -= p.price;
+           add(p);
        }
    }
 
    public void add(Product p){
-
+        if (i >= cart.length){
+            Product[] tmp = new Product[cart.length*2];
+            for (int j = 0; j<cart.length; j++){
+                tmp[j] = cart[j];
+            }
+            cart = tmp;
+        }
+        cart[i] = p;
+        i++;
    }
 
    public void summary(){
-
+       String s ="";
+       for (int j=0; j<i; j++){
+           s += cart[j].toString() + ", ";
+       }
+       System.out.printf("구입한 물건:%s\n", s);
+       System.out.println("사용한 금액:"+(1000-money));
+       System.out.println("남은 금액:"+money);
    }
 
 
