effb636f0eeaf362cf4cea7f9d118808da93c7b1
aksmf1442 <64648893+aksmf1442@users.noreply.github.com>
2021-01-03 10:58:04 +0900

    Step3) Direction Class Init

---
+++ Java_Cube/src/step3/Direction.java
@@ -0,0 +1,13 @@
+package step3;
+
+import java.util.ArrayList;
+
+public class Direction {
+    String directionInit;
+    ArrayList<String> direction;
+
+    public Direction(){
+        direction = new ArrayList<>();
+    }
+    
+}
