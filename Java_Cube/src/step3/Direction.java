7dba31ba1843492231fe8058069bd9e57d14e700
aksmf1442 <64648893+aksmf1442@users.noreply.github.com>
2021-01-03 11:08:32 +0900

    Step3) DirectionClass replaceDirection function add

--- Java_Cube/src/step3/Direction.java
+++ Java_Cube/src/step3/Direction.java
@@ -6,8 +6,22 @@ public class Direction {
     String directionInit;
     ArrayList<String> direction;
 
-    public Direction(){
+    public Direction(String directionInit){
+        this.directionInit = directionInit;
+        replaceDirection();
         direction = new ArrayList<>();
     }
+
+    private void replaceDirection(){
+        directionInit = directionInit.replace("U'","u");
+        directionInit = directionInit.replace("D'","d");
+        directionInit = directionInit.replace("R'","r");
+        directionInit = directionInit.replace("L'","l");
+        directionInit = directionInit.replace("F'","f");
+        directionInit = directionInit.replace("B'","b");
+    }
+
     
+
+
 }
