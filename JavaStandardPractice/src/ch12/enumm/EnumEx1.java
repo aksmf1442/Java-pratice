2a07b8a46446b9726f034b406fafa5d919efd634
aksmf1442 <64648893+aksmf1442@users.noreply.github.com>
2021-02-09 11:40:11 +0900

    자바의 정석 enum공부

---
+++ JavaStandardPractice/src/ch12/enumm/EnumEx1.java
@@ -0,0 +1,25 @@
+package ch12.enumm;
+
+enum Direction {EAST, SOUTH, WEST, NORTH}
+
+public class EnumEx1 {
+
+  public static void main(String[] args) {
+    Direction d1 = Direction.EAST;
+    Direction d2 = Direction.valueOf("WEST");
+    Direction d3 = Enum.valueOf(Direction.class, "EAST");
+
+    System.out.println("d1=" + d1);
+    System.out.println("d2=" + d2);
+    System.out.println("d3=" + d3);
+
+    System.out.println("d1==d2 ? " + (d1==d2));
+    System.out.println("d1==d3 ? " + (d1==d3));
+    System.out.println("d1.eqauls(d3) ? " + (d1.equals(d3)));
+//    System.out.println("d2 > d3 ? " + (d2 > d3));  에러 발생 >,<연산자 사용불가
+    System.out.println("d1.compareTo(d3) ? " + (d1.compareTo(d3)));
+    System.out.println("d1.compareTo(d2) ? " + (d1.compareTo(d2)));
+
+    
+  }
+}
--- JavaStandardPractice/src/chapter16/TcpIpServer.java
+++ JavaStandardPractice/src/chapter16/TcpIpServer.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketTimeoutException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -26,6 +27,7 @@ public class TcpIpServer {
         System.out.println(getTime() + "연결요청을 기다립니다.");
         // 서버소켓은 클라이언트의 연결요청이 올 때까지 실행을 멈추고 계속 기다린다.
         // 클라이언트의 연결요청이 오면 클라이언트 소켓과 통신한 새로운 소켓을 생성한다.
+        serverSocket.setSoTimeout(10*1000);
         Socket socket = serverSocket.accept();
         System.out.println(getTime() + socket.getInetAddress() + "로부터 연결요청이 들어왔습니다.");
 
@@ -40,7 +42,11 @@ public class TcpIpServer {
         // 스트림과 소켓을 닫아준다.
         dos.close();
         socket.close();
-      } catch (IOException e) {
+      }catch (SocketTimeoutException e){
+        System.out.println("지정된 시간동안 접속요청이 없어서 서버를 종료합니다.");
+        System.exit(0);
+      }
+      catch (IOException e) {
         e.printStackTrace();
       }
     }
